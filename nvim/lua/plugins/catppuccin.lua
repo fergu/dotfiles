12f9646bc9f8ef117027c47d7ce69ab9e5ed537a
Kevin <9022843+fergu@users.noreply.github.com>
2024-10-14 21:04:52 -0700

    Fixed integration with barbar in configuration

--- nvim/lua/plugins/catppuccin.lua
+++ nvim/lua/plugins/catppuccin.lua
@@ -2,5 +2,11 @@ return {
 	"catppuccin/nvim", 
 	name = "catppuccin", 
 	priority = 1000,
-	barbar = true,
+	config = function()
+		require("catppuccin").setup {
+			integrations = {
+				barbar = true,
+			},
+		}
+	end,
 }
