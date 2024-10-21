0386e1e45f3d8736ce5ce24d32767beef71ed337
Kevin <9022843+fergu@users.noreply.github.com>
2024-10-20 20:37:20 -0700

    Changed file layout

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,1 @@
-require("fergu")		-- The "./lua/fergu" directory contains configuration specific to me. We load this first as things like mapleader need to be defined *before* lazy is loaded
-require("lazy_init") 	-- Install (if needed) and load the Lazy package manager, with packages located in the "./lua/lazy_init/plugins" directory
-
--- One of the last things we want to do is set our colorscheme (which will only be availble after lazy starts, so we do it here
-vim.cmd.colorscheme "catppuccin"
+require("fergu")		-- Loads configuration from the ./lua/fergu directory
