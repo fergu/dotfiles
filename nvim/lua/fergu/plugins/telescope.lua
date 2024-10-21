7b17894b0cf71674306589e92dd881467f112947
Kevin <9022843+fergu@users.noreply.github.com>
2024-10-20 20:36:19 -0700

    Added telescope

---
+++ nvim/lua/fergu/plugins/telescope.lua
@@ -0,0 +1,33 @@
+return {
+	'nvim-telescope/telescope.nvim',
+	branch = '0.1.x',
+	dependencies = { 
+		'nvim-lua/plenary.nvim',
+		{ "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+    	"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		telescope = require("telescope")
+		actions = require("telescope.actions")
+
+		telescope.setup({
+			defaults = {
+				path_display = { "smart" },
+				mappings = {
+					i = {
+						["<C-k>"] = actions.move_selection_previous, -- move to prev result
+						["<C-j>"] = actions.move_selection_next, -- move to next result
+					},
+				},
+			},
+		})
+
+		telescope.load_extension("fzf")
+
+		-- set keymaps
+		vim.keymap.set("n", "<leader>ff", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
+		vim.keymap.set("n", "<leader>fr", "<cmd>Telescope oldfiles<cr>", { desc = "Fuzzy find recent files" })
+		vim.keymap.set("n", "<leader>fg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
+		vim.keymap.set("n", "<leader>fs", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+	end,
+}
