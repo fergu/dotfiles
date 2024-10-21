2db7da090f74ea0819a33e9dd43fc6a51db54fc5
Kevin <9022843+fergu@users.noreply.github.com>
2024-10-20 20:36:37 -0700

    Added keybinds to nvim tree

--- nvim/lua/fergu/plugins/nvim-tree.lua
+++ nvim/lua/fergu/plugins/nvim-tree.lua
@@ -6,6 +6,16 @@ return {
     "nvim-tree/nvim-web-devicons",
   },
   config = function()
-    require("nvim-tree").setup {}
+	  -- Settings recommended by nvim-tree devs
+	  vim.g.loaded_netrw = 1
+	  vim.g.loaded_netrwPlugin = 1
+
+	  require("nvim-tree").setup()
+
+	  -- Set up keybinds
+	  vim.keymap.set("n", "<leader>tt", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
+	  vim.keymap.set("n", "<leader>tf", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
+	  vim.keymap.set("n", "<leader>tc", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+	  vim.keymap.set("n", "<leader>tr", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
   end,
 }
